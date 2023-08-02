cceca7c308a8c144058afb498464cdf6890d7348
skywarth <yigitk.ersoy@gmail.com>
2023-08-02 23:56:07 +0300

    WIP: working on seed generation and command acquisition from scheduling event

--- src/Services/ChaoticSchedule.php
+++ src/Services/ChaoticSchedule.php
@@ -16,7 +16,7 @@ class ChaoticSchedule
 
     public function __construct()
     {
-        $this->rng=RNGFactory::getRngEngine(17);
+        $this->rng=RNGFactory::getRngEngine();
     }
 
     public function registerMacros(){
@@ -32,43 +32,61 @@ $minute = $twister->rangeint(0,1440) % 60;*/
         //TODO fix outer this access, maybe get rid of anonymous function
         $chaoticSchedule=$this;
         Event::macro('atRandom', function (string $minTime, string $maxTime) use($chaoticSchedule){
+            //Laravel automatically injects and replaces $this in the context
 
+            return $chaoticSchedule->randomTimeSchedule($this,$minTime,$maxTime);
 
-            //H:i is 24 hour format
-            $minTimeCasted=Carbon::createFromFormat('H:i',$minTime);
-            $maxTimeCasted=Carbon::createFromFormat('H:i',$maxTime);
-            //$maxTimeCasted=strtotime($maxTime);
-
-            /*
-           //cancelling below for now, I don't think it would provide homogenous distribution.
-           $this->rng->intBetween($minTimeCasted,$maxTimeCasted);;
-           */
-
-            $minMinuteOfTheDay=($minTimeCasted->hour * 60) + $minTimeCasted->minute;
-            $maxMinuteOfTheDay=($maxTimeCasted->hour * 60) + $maxTimeCasted->minute;
-
-
-            $randomMOTD=$chaoticSchedule->getRng()->intBetween($minMinuteOfTheDay,$maxMinuteOfTheDay);
-
-            $designatedHour=$randomMOTD/60;
-            $designatedMinute=$randomMOTD%60;
-
-
-            $this->at("$designatedHour:$designatedMinute");
-
-            //do ya thing
-            return $this;
         });
+    }
+
+    public function randomTimeSchedule(Event $schedule,string $minTime, string $maxTime):Event{
+
+
+
+        $this->getRng()->setSeed($this->getSeeder()->seedForDay('replace-me-with-command'));
+        //H:i is 24 hour format
+        $minTimeCasted=Carbon::createFromFormat('H:i',$minTime);
+        $maxTimeCasted=Carbon::createFromFormat('H:i',$maxTime);
+        //$maxTimeCasted=strtotime($maxTime);
+
+        /*
+       //cancelling below for now, I don't think it would provide homogenous distribution.
+       $this->rng->intBetween($minTimeCasted,$maxTimeCasted);;
+       */
+
+        $minMinuteOfTheDay=($minTimeCasted->hour * 60) + $minTimeCasted->minute;
+        $maxMinuteOfTheDay=($maxTimeCasted->hour * 60) + $maxTimeCasted->minute;
+
+
+        $randomMOTD=$this->getRng()->intBetween($minMinuteOfTheDay,$maxMinuteOfTheDay);
+
+        $designatedHour=$randomMOTD/60;
+        $designatedMinute=$randomMOTD%60;
+
+
+        $schedule->at("$designatedHour:$designatedMinute");
+
+        return $schedule;
     }
 
     /**
      * @return RandomNumberGeneratorAdapter
      */
-    public function getRng(): RandomNumberGeneratorAdapter
+    protected function getRng(): RandomNumberGeneratorAdapter
     {
         //TODO: we shouldn't have this accessor, why would adapter be exposed ?
         return $this->rng;
     }
+
+    /**
+     * @return SeedGenerationService
+     */
+    protected function getSeeder(): SeedGenerationService
+    {
+        return $this->seeder;
+    }
+
+
 
 
 
--- src/Services/SeedGenerationService.php
+++ src/Services/SeedGenerationService.php
@@ -4,12 +4,17 @@ namespace Skywarth\ChaoticSchedule\Services;
 
 class SeedGenerationService
 {
-    public function seedForDay():int{
-        return intval(date('Ymd'));
+    public function seedForDay(string $uniqueIdentifier):int{
+        return intval(date('Ymd').$this->castUniqueIdentifier($uniqueIdentifier));
     }
 
-    public function seedForWeek():int{
-        return intval(date('YW'));
+    public function seedForWeek(string $uniqueIdentifier):int{
+        return intval(date('YW').$this->castUniqueIdentifier($uniqueIdentifier));
+    }
+
+
+    protected function castUniqueIdentifier(string $uniqueIdentifier){
+        return crc32($uniqueIdentifier);
     }
 
 }
